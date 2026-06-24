e00d8e4235df74a57708c03e7a61e650308700c1
Asragen <asragenoff@gmail.com>
2026-06-24 14:17:41 +0530

    Configure using next.config.js for CommonJS compatibility with GitHub Pages auto-patcher

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/Asragen',
   images: {
     unoptimized: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
